4d864f95cb7aac803345e64e513b26d0f24e8fdc
mvjl000 <milosz.devmail@gmail.com>
2021-04-14 13:05:25 +0200

    Routes and basic styles

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Róznorodne i bogate doświadczenia, zakres i miejsce szkolenia kadr
+        spełnia ważne zadania w wypracowaniu form oddziaływania. Koleżanki i
+        koledzy, realizacja nakreślonych zadań programowych uniemożliwia w
+        większym stopniu tworzenie nowych propozycji. Praktyka dnia codziennego
+        dowodzi że, realizacja nakreślonych zadań programowych
+      </p>
+    </div>
+  );
+};
+
+export default About;
