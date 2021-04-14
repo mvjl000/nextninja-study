811f5013d1073a795ec584d898337cd5e9ca43d7
mvjl000 <milosz.devmail@gmail.com>
2021-04-14 16:50:17 +0200

    Rendering all ninjas :D

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,6 +1,19 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../../styles/Ninjas.module.css';
 
-const Ninjas = () => {
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: {
+      ninjas: data,
+    },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
   return (
     <>
       <Head>
@@ -8,6 +21,13 @@ const Ninjas = () => {
       </Head>
       <div>
         <h1>All Ninjas</h1>
+        {ninjas.map((ninja) => (
+          <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
+            <a className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </Link>
+        ))}
       </div>
     </>
   );
